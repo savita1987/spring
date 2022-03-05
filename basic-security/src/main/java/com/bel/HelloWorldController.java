2ed268f7924415dbb13dcf20aa57fed3b52e0d0d
savita <savita.ssb@gmail.com>
2022-03-05 14:49:12 +0530

    final

---
+++ basic-security/src/main/java/com/bel/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.bel;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	//http://localhost:8080/hello?name=User
+	
+		@GetMapping("/hello")
+		public String sayHello(@RequestParam("name") String name) {
+			return "Hello "+name;
+		}
+}
